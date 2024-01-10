668a44fd9645aa72b857d7eb86d763f32cb16567
Andre <andre.luismp@gmail.com>
2024-01-10 15:39:27 +0000

    router and controller mock

---
+++ server-Fat-Cow/controllers/fatcow.controller.js
@@ -0,0 +1,5 @@
+
+
+export const getNutriInfo = function () {
+
+}
--- server-Fat-Cow/router.js
+++ server-Fat-Cow/router.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-
+import { getNutriInfo } from './controllers/fatcow.controller.js';
 const router = Router();
 
-
+router.get('/nutri-info', getNutriInfo);
 
 export default router;
